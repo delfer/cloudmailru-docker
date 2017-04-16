dd0069554c6356c6596031af025ef81efe41ce6d
Delfer <i@delfer.ru>
2017-04-16 16:34:33 +0300

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash    
+
+/usr/bin/expect <<EOF
+set timeout 60
+spawn /usr/bin/vncpasswd
+expect "Password:"
+send "$VNCPASS\r"
+expect "Verify:"
+send "$VNCPASS\r"
+expect eof
+EOF
+
+rm -rf /tmp/.X*-lock /tmp/.X11-unix
+vnc4server $DISPLAY &&
+cloud
